36ff98d4e802f1c7a35401d83618940092ab4684
밤밤 <jiiyo0@naver.com>
2024-01-04 21:10:53 +0900

    Feat: 계산기 기능 구현

---
+++ calculator/index.js
@@ -0,0 +1,27 @@
+// 결과를 표시할 input 요소
+const input = document.querySelector("input");
+
+function appendToResult(value) {
+	if (input.value === "0") {
+		input.value = value;
+	} else {
+		input.value += value;
+	}
+}
+
+// 결과 계산
+function calculateResult() {
+	try {
+		let expression = input.value;
+		let answer = eval(expression);
+		answer = Math.floor(answer); // 소수점 이하는 버리기
+		input.value = answer;
+	} catch (error) {
+		input.value = "Error"; // 계산 중 오류 발생하면 에러
+	}
+}
+
+// 결과 초기화
+function clearResult() {
+	input.value = "0";
+}
